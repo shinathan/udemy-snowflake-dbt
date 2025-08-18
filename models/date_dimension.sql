7d6e4c0095de3f2427e03fac994ecf55b195f796
Hsiao Kee Shi <nathanshi@fastmail.com>
2025-08-18 11:28:36 +0000

    tdyj

---
+++ models/date_dimension.sql
@@ -0,0 +1,16 @@
+with cte as (
+    select
+    TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+    DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+    HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+    {{day_type('STARTED_AT')}} as DAY_TYPE,
+    {{get_season('STARTED_AT')}} as SEASON_OF_YEAR
+
+    from
+    {{ source('demo', 'bike') }}
+    limit 999999 offset 1
+)
+
+select
+*
+from cte
